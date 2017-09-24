dd200d8eb5be548f5ab16c98d15c4bb655be4c70
xou816 <trendel.alexandre@gmail.com>
2017-09-24 20:14:55 +0200

    Fix app crashing on timeout

--- src/ldap.js
+++ src/ldap.js
@@ -3,11 +3,14 @@ const client = ldap.createClient({
 	url: 'ldaps://130.66.41.27:636',
 	tlsOptions: {
 		rejectUnauthorized: false
-	}
+	},
+	timeout: 3000,
+	connectTimeout: 3000
 });
 
 module.exports = function(username, password) {
 	return new Promise((resolve, reject) => {
+		client.on('error', err => reject());
 		if (/\w+/.test(username)) {
 			client.bind('uid=' + username + ',ou=people,dc=ec-nantes,dc=fr', password, (err) => {
 				if (err === null) {
