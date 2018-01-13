3f3cb06889250e698787dd9c76222e36b1d31646
Alexandre Trendel <trendel.alexandre@gmail.com>
2018-01-13 16:09:02 +0100

    update filter and calendar

--- src/calendar.ts
+++ src/calendar.ts
@@ -1,83 +1,99 @@
-const ical = require('ical-generator');
-const request = require('request-promise');
-const libxmljs = require('libxmljs');
-const moment = require('moment-timezone');
-const crypto = require('crypto');
-const Filter = require('./filter');
+import {Moment, tz} from 'moment-timezone';
+import * as ical from 'ical-generator';
+import * as request from 'request-promise';
+import fetch from 'node-fetch';
+import {createHash} from 'crypto';
+import {parseXml, Element} from 'libxmljs';
+import {Filter} from './filter';
 
 const FILTER = 'Groupe';
 const WARN_MESSAGE = "(!) Ce calendrier n'est peut être pas à jour. Les filtres sont désactivés par sécurité."
 
-const dateFromCourseTime = function(date, hour) {
-	let parsed = hour.split(':').map((i) => parseInt(i, 10));
-	let new_date = moment(date);
-	new_date.hours(parsed[0]);
-	new_date.minutes(parsed[1]);
-	return new_date;
+export type CalendarId = {
+	id: string,
+	name: string,
+	display: string
 };
 
-const safeGet = function(node, xpaths, fallback) {
-	fallback = typeof fallback === 'undefined' ? '' : fallback;
-	let res = fallback;
-	xpaths.some((xpath) => {
-		try {
-			res = node.get(xpath).text();
-			return true;
-		} catch (e) {
-			return false;
-		}
-	});
-	return res;
+export type CalendarEvent = {
+	start: Moment,
+	end: Moment,
+	subject: string,
+	full_subject: string,
+	location: string,
+	description: string,
+	organizer: string
 };
 
-exports.listOnlineCalendars = function(type) {
-	if (typeof type === 'undefined') {
-		type = 'g';
-	}
-	let url = 'http://website.ec-nantes.fr/sites/edtemps/finder.xml';
-	return request(url).then(function(body) {
-		let doc = libxmljs.parseXml(body);
-		return doc.find('/finder/resource')
-			.filter((node) => node.get('link').attr('href').value()[0] === type)
-			.map((node) => {
-				let names = node.get('name').text().split(',');
-				let id = node.get('link').attr('href').value().split('.').shift();
+export type Calendar = CalendarEvent[];
+
+export type Subjects = {[key: string]: number};
+
+const calendarList = 'http://website.ec-nantes.fr/sites/edtemps/finder.xml';
+const calendarUrl = (id: string) => `http://website.ec-nantes.fr/sites/edtemps/${id}.xml`;
+
+export function listOnlineCalendars(letter: string = 'g'): Promise<CalendarId[]> {
+	return fetch(calendarList)
+		.then(res => res.text())
+		.then(body => parseXml(body))
+		.then(doc => doc.find('/finder/resource')
+			.map(node => ({
+				link: node.get('link'),
+				names: node.get('names')
+			}))
+			.filter(node => node.link != null && node.names != null)
+			.map(node => {
+				let names = node.names!.text().split(',');
+				let id = node.link!.attr('href').value().split('.').shift();
 				return {
-					id: id,
+					id: id!,
 					name: names[0].trim(),
-					display: typeof names[1] !== 'undefined' ? names[1].trim() : ''
+					display: (names[1] || '').trim()
 				};
-			});
-	});
-};
+			}));
+}
 
-exports.getIdFromName = function(name) {
-	return exports.listOnlineCalendars()
-		.then((calendars) => calendars.find((cal) => cal.name.toLowerCase() === name.toLowerCase()))
-		.then((cal) => cal.id);
-};
+export function getIdFromName(name: string): Promise<string|null> {
+	return listOnlineCalendars()
+		.then(calendars => calendars
+			.find(cal => cal.name.toLowerCase() === name.toLowerCase()))
+		.then(cal => cal == null ? null : cal.id);
+}
 
-const mapNodeToCalendar = function(node, dates) {
+function safeText(node: Element, xpaths: string[], fallback: string = ''): string {
+	let res = fallback;
+	let found: string|null = xpaths.reduce((res: string|null, xpath: string) => {
+		return res == null ? (node.get(xpath) || {text: () => null}).text() : res;
+	}, null);
+	return found || fallback; 
+}
+
+function mapNodeToEvent(node: Element, weekNumToFirstDay: string[][]): CalendarEvent {
+
+	let day = parseInt(safeText(node, ['day']), 10);
+	let week = parseInt(safeText(node, ['prettyweeks']), 10);
+	let date = weekNumToFirstDay[week][day];
+	let description = safeText(node, ['notes']);
+	let organizer = safeText(node, ['resources/staff/item']).split(' ').shift() || '';
+	
 	const reg = /(\w+) \(\1\)/i;
-	let day = parseInt(node.get('day').text(), 10);
-	let week = parseInt(node.get('prettyweeks').text(), 10);
-	let date = dates[week][day];
-	let description = safeGet(node, ['notes']);
-	let organizer = safeGet(node, ['resources/staff/item']).split(' ').shift();
-	let subject = safeGet(node, ['resources/module/item', 'notes']).split('-').shift();
+
+	let subject = safeText(node, ['resources/module/item', 'notes']).split('-').shift() || '';
 	if (reg.test(subject)) {
-		subject = subject.split(' ').shift();
+		subject = subject.split(' ').shift()!;
 	}
-	let location = safeGet(node, ['resources/room/item']);
+	let cat = safeText(node, ['category']);
+	let full_subject = cat + ' ' + subject;
+
+	let location = safeText(node, ['resources/room/item']);
 	if (reg.test(location)) {
-		location = location.split(' ').shift();
+		location = location.split(' ').shift()!;
 	}
-	subject = subject.trim();
-	let full_subject = safeGet(node, ['category']) + ' ' + subject;
+
 	return {
-		start: dateFromCourseTime(date, node.get('starttime').text()),
-		end: dateFromCourseTime(date, node.get('endtime').text()),
-		subject: subject,
+		start: dateFromCourseTime(date, safeText(node, ['starttime'])),
+		end: dateFromCourseTime(date, safeText(node, ['endtime'])),
+		subject: subject.trim(),
 		full_subject: full_subject.trim(),
 		location: location,
 		description: description,
@@ -85,73 +101,69 @@ const mapNodeToCalendar = function(node, dates) {
 	}
 }
 
-exports.getOnlineCalendar = function(id) {
-	const url = 'http://website.ec-nantes.fr/sites/edtemps/' + id + '.xml';
-	return request(url).then(function(body) {
-		let doc = libxmljs.parseXml(body);
-		let dates = [];
-		doc.find('/timetable/span').map((node) => {
-			let index = parseInt(node.get('title').text(), 10);
-			dates[index] = node.find('day/date').map((date) => moment.tz(date, 'DD/MM/YYYY', 'Europe/Paris'));
-		});
-		return doc.find('//event')
-			.map(node => mapNodeToCalendar(node, dates))
-			.sort((a, b) => a.start - b.start);
-	});
-};
+function dateFromCourseTime(date: string, hour: string) {
+	return moment.tz(`${date} ${hour}`, 'DD/MM/YYYY hh:mm', 'Europe/Paris');
+}
 
-exports.getSubjects = function(events) {
+export function getOnlineCalendar(id: string): Promise<Calendar> {
+	return fetch(calendarUrl(id))
+		.then(res => res.text())
+		.then(body => parseXml(body))
+		.then(doc => {
+			let dates: string[][] = doc.find('/timetable/span').reduce((acc: string[][], node: Element) => {
+				let index = parseInt((node.get('title') || {text: () => ''}).text(), 10);
+				acc[index] = node.find('day/date').map(date => date.text());
+				return acc;
+			}, []);
+			return doc.find('//event')
+				.map(node => mapNodeToEvent(node, dates))
+				.sort((a, b) => a.start.valueOf() - b.start.valueOf());
+		});
+}
+
+export function getSubjects(events: Calendar): Subjects {
 	return events
-		.map((e) => e = { subject: e.subject, date: e.start })
+		.map(e => ({ subject: e.subject, date: e.start })) // todo improve double sort
 		.sort((a, b) => {
 			let bool = a.subject < b.subject || a.subject == b.subject && a.date < b.date
 			return bool ? -1 : 1;
 		})
 		.filter((e, pos, arr) => (e.subject.length && (!pos || e.subject != arr[pos - 1].subject)))
 		.sort((a, b) => (a.date < b.date ? -1 : 1))
-		.reduce((final, e, index) => {
-			final[e.subject] = index;
-			return final;
-		}, {});
-};
+		.reduce((final, e, index) => ({
+			...final,
+			[e.subject]: index
+		}), {});
+}
 
-const getSimpleCustomCalendar = function(id) {
+function getSingleCustomCalendar(id: string): Promise<Calendar> {
 	let [calid, filter_withsum] = id.split('-');
-	if (typeof filter_withsum === 'undefined') {
-		return exports.getOnlineCalendar(calid);
+	if (filter_withsum == null) {
+		return getOnlineCalendar(calid);
 	}
 	let [filter_enc, checksum] = filter_withsum.split('_');
 	let filter = Filter.parse(filter_enc);
-	return exports.getOnlineCalendar(calid)
-		.then((events) => {
-			let subjects = exports.getSubjects(events);
-			let warn = typeof checksum !== 'undefined' && checkSubjects(filter, subjects, checksum);
-			if (warn) {
-				return events.map(e => Object.assign(e, {
-						description: e.description + WARN_MESSAGE
-					}));
-			}
-			return events
-				.filter((event) => {
-					let pos = subjects[event.subject];
-					return filter.test(pos);
-				});
+	return getOnlineCalendar(calid)
+		.then(events => {
+			let subjects = getSubjects(events);
+			let warn = checksum != null && checkSubjects(filter, subjects, checksum);
+			return warn ? 
+				events.map(event => ({...event, description: event.description + WARN_MESSAGE})) : 
+				events.filter(event => filter.test(subjects[event.subject]));
 		});
 };
 
-exports.getCustomCalendar = function(id) {
-	return Promise.all(id
-			.split('+')
-			.map(getSimpleCustomCalendar))
+export function getCustomCalendar(id: string): Promise<Calendar> {
+	return Promise.all(id.split('+').map(getSingleCustomCalendar))
 		.then(all => all.reduce((acc, events) => acc.concat(events), []));
-};
+}
 
-const makeChecksum = function(indexed_subjects, length) {
-	let str = Object.keys(indexed_subjects).slice(0, length).join(',');
-	return crypto.createHash('sha1').update(str).digest('hex').substr(0, 6);
-};
+function makeChecksum(subjects: Subjects, length: number): string {
+	let str = Object.keys(subjects).slice(0, length).join(',');
+	return createHash('sha1').update(str).digest('hex').substr(0, 6);
+}
 
-const checkSubjects = function(filter, subjects, checksum) {
+function checkSubjects(filter: Filter, subjects: Subjects, checksum: string): boolean {
 	let length;
 	if (checksum.indexOf('l') > -1) { // old checksum format!
 		let [l, c] = checksum.split('l');
@@ -161,21 +173,21 @@ const checkSubjects = function(filter, subjects, checksum) {
 		length = filter.length();
 	}
 	return checksum !== makeChecksum(subjects, length);
-};
+}
 
-exports.createFilter = function(id, indices, subjects) {
+export function createFilter(id: string, indices: number[], subjects: Subjects): string {
 	let filter = Filter.from(indices);
 	return id + '-' + filter.toString() + '_' + makeChecksum(subjects, filter.length());
-};
+}
 
-exports.calendarToIcs = function(events) {
+export function calendarToIcs(events: Calendar): string {
 	let cal = ical({
         domain: 'ec-nantes.fr',
         name: 'EDT',
         timezone: 'Europe/Paris',
-        prodId: { company: 'ec-nantes.fr', product: 'edt' },
+        prodId: {company: 'ec-nantes.fr', product: 'edt'},
     });
-	events.forEach((event) => {
+	events.forEach(event => {
 		cal.createEvent({
 			start: event.start.tz('UTC').toDate(),
 			end: event.end.tz('UTC').toDate(),
--- src/filter.ts
+++ src/filter.ts
@@ -1,24 +1,23 @@
-class Filter {
+export class Filter {
 
-	static get TARGET_BASE() {
-		return 32;
+	static TARGET_BASE = 32;
+	static MAX_BITS = 28;
+	static STR_LEN = Math.ceil(Filter.MAX_BITS/Math.log2(Filter.TARGET_BASE));
+
+	private filters: number[];
+
+	static parse(input: string): Filter {
+		const reg = new RegExp(`[a-z0-9]{1,${Filter.STR_LEN}}`, 'gi');
+		let matches = input.match(reg);
+		if (matches !== null) {
+			let filters = matches.map(encoded => parseInt(encoded, Filter.TARGET_BASE));
+			return new Filter(filters);
+		} else {
+			throw new Error('invalid filter');
+		}
 	}
 
-	static get MAX_BITS() {
-		return 28;
-	}
-
-	static get STR_LEN() {
-		return Math.ceil(Filter.MAX_BITS/Math.log2(Filter.TARGET_BASE));
-	}
-
-	static parse(input) {
-		const reg = new RegExp('[a-z0-9]{1,' + Filter.STR_LEN + '}', 'gi');
-		let filters = input.match(reg).map((encoded) => parseInt(encoded, Filter.TARGET_BASE));
-		return new Filter(filters);
-	}
-
-	static from(indices) {
+	static from(indices: number[]): Filter {
 		let max = Math.max.apply(null, indices) + 1;
 		let filters = new Array(Math.ceil(max/Filter.MAX_BITS));
 		filters.fill(0);
@@ -30,28 +29,26 @@ class Filter {
 		return new Filter(filters);
 	}
 
-	constructor(filters) {
+	constructor(filters: number[]) {
 		this.filters = filters;
 	}
 
-	length() {
+	length(): number {
 		let l = this.filters.length;
 		return (l-1)*Filter.MAX_BITS + Math.ceil(Math.log2(this.filters[l-1]));
 	}
 
-	toString() {
+	toString(): string {
 		return this.filters
 			.map(num => num.toString(Filter.TARGET_BASE))
 			.join('');
 	}
 
-	test(index) {
+	test(index: number): boolean {
 		let filter = this.filters[Math.floor(index/Filter.MAX_BITS)];
-		filter = (typeof filter === 'undefined') ? 0 : filter;
+		filter = filter == null ? 0 : filter;
 		let bin = (1 << (index%Filter.MAX_BITS)) & filter;
-		return bin == 0;
+		return bin === 0;
 	}
 
 }
-
-module.exports = Filter;
